85d7aad66812ad38c1a28c34f30451b4ffe7aeee
cryptothanh <115431178+cryptothanh@users.noreply.github.com>
2022-11-05 23:38:46 +0100

    Update setteg.js

--- setteg.js
+++ setteg.js
@@ -1,7 +1,7 @@
 const MAX=5
-const namess="Trippy Ape Yacht Club";
-const discord = 'https://discord.gg/tayceth';
-const twitter = 'https://twitter.com/tayceth';
+const namess="Baby Y00ts";
+const discord = 'https://discord.gg/babyy00ts';
+const twitter = 'https://twitter.com/babyy00ts';
 const startCounter = 2836;
 const endCounter = 3333;
 const IMGPrice=0.005;
