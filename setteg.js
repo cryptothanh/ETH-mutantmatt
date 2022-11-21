1fbb1fc702df25ded0370d5129d1f1da826f658c
cryptothanh <115431178+cryptothanh@users.noreply.github.com>
2022-11-22 00:20:40 +0100

    Update setteg.js

--- setteg.js
+++ setteg.js
@@ -1,9 +1,9 @@
 const MAX=5
-const namess="Operation Mitsubishi";
-const discord = 'https://discord.gg/mitsubishi';
-const twitter = 'https://twitter.com/mitsubishi';
+const namess="Baby Y00ts";
+const discord = 'https://discord.gg/babyy00ts';
+const twitter = 'https://twitter.com/babyy00ts';
 const startCounter = 3186;
 const endCounter = 3333;
 const IMGPrice=0.005;
-const ADDRESS = '0xaFF52f41E87856522aced78A6859950946EE287D';
+const ADDRESS = '0x937c1859B66c1eC879fC56EfC31f18826522238c';
 
